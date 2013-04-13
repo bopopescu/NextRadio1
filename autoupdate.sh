6455d4e3a05e4cd0d2e59a67ccc9f577f7b14450
Aladdin Chang <sp0500@gmail.com>
2013-04-13 16:32:30 +0800

    Add autoupdate.sh and radio-start.py

---
+++ autoupdate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# file: autoupdate.sh
+#
+
+#
+WGET='/usr/bin/wget --no-check-certificate -N'
+
+# Updating the a.py
+URL='https://github.com/sp0500/NextRadio/raw/master/a.py'
+$WGET $URL
+[ $? = 0 ] && echo "Updating the file a.py Done."
+
+# Adding the updated scripts for NextRadio to the following
