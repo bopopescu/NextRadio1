ed663477f9e9c91af709e62de3953b8d4a3c435c
Aladdin Chang <sp0500@gmail.com>
2013-04-22 19:43:21 +0800

    update a.1.py autoupdate.sh radio.pls from A-Lang

--- autoupdate.sh
+++ autoupdate.sh
@@ -11,3 +11,9 @@ $WGET $URL
 [ $? = 0 ] && echo "Updating the file a.py Done."
 
 # Adding the updated scripts for NextRadio to the following
+#
+[ -r ./radio.pls ] || {
+   URL='https://github.com/sp0500/NextRadio/raw/master/radio.pls'
+   $WGET $URL
+   [ $? = 0 ] && echo "Updating the file radio.pls Done."
+}
